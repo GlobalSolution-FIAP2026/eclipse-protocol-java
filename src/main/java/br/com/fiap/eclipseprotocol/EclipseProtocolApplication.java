33b2966cb87db24a775b1a935320161ad7d2ac5c
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-05-28 20:49:47 -0300

    first commit

---
+++ src/main/java/br/com/fiap/eclipseprotocol/EclipseProtocolApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.eclipseprotocol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclipseProtocolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclipseProtocolApplication.class, args);
+	}
+
+}
