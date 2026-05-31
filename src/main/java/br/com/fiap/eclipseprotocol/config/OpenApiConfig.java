8562f3517b6f8f6dbbfb60d5ac22443585050169
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-05-31 18:42:40 -0300

    feat: adicionado o bearer para adicionar token

--- src/main/java/br/com/fiap/eclipseprotocol/config/OpenApiConfig.java
+++ src/main/java/br/com/fiap/eclipseprotocol/config/OpenApiConfig.java
@@ -3,21 +3,41 @@ package br.com.fiap.eclipseprotocol.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
     @Bean
     public OpenAPI customOpenAPI() {
+
         return new OpenAPI()
-                .info(new Info()
-                        .title("Eclipse Protocol API")
-                        .version("1.0")
-                        .description("API para monitoramento inteligente de plantações agrícolas utilizando sensores IoT, leituras ambientais e geração de alertas.")
-                        .contact(new Contact()
-                                .name("Equipe Eclipse Protocol")
-                                .email("contato@eclipseprotocol.com")));
+                .info(
+                        new Info()
+                                .title("Eclipse Protocol API")
+                                .version("1.0")
+                                .description("API para monitoramento inteligente de plantações agrícolas utilizando sensores IoT, leituras ambientais e geração de alertas.")
+                                .contact(
+                                        new Contact()
+                                                .name("Equipe Eclipse Protocol")
+                                )
+                )
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(SECURITY_SCHEME_NAME)
+                )
+                .schemaRequirement(
+                        SECURITY_SCHEME_NAME,
+                        new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                );
     }
 }
